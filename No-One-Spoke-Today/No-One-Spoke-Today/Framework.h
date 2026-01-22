d8f8c3731ad8760d58377bb89a87d7648ebc9fcf
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2026-01-22 18:19:27 +0900

    프레임 워크 및 타임 클래스 생성

---
+++ No-One-Spoke-Today/No-One-Spoke-Today/Framework.h
@@ -0,0 +1,10 @@
+#pragma once
+class Framework
+{
+public:
+	Framework();
+	~Framework();
+private:
+
+};
+
---
+++ No-One-Spoke-Today/No-One-Spoke-Today/pch.h
@@ -0,0 +1,20 @@
+// pch.h or Common.h
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <functional>
+#include <optional>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cassert>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
