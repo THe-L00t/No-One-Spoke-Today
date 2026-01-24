f360a144a81952a658e9b9cb6a5d4cbd04414f35
leeth <sigun2001@naver.com>
2026-01-24 19:11:04 +0900

    분리 씬 클래스 선언

---
+++ No-One-Spoke-Today/No-One-Spoke-Today/Scene.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "pch.h"
+class Scene
+{
+public:
+
+private:
+
+};
+
+class TitleScene : public Scene 
+{
+
+};
+
+class GameScene : public Scene
+{
+
+};
+
+class MenuScene : public Scene
+{
+
+};
