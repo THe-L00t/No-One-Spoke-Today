317781113fd9e8badd0336e9309e31dd0c821d45
leeth <sigun2001@naver.com>
2026-01-24 14:02:42 +0900

    time 클래스 정의

--- No-One-Spoke-Today/No-One-Spoke-Today/Time.h
+++ No-One-Spoke-Today/No-One-Spoke-Today/Time.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <chrono>
 class Time
 {
+public:
+	static Time* Instance;
+
+public:
+	Time()
+	{
+		if (Instance == nullptr) Instance = this;
+		startTime = std::chrono::high_resolution_clock::now();
+		prevTime = startTime;
+	}
+	void Update()
+	{
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - prevTime);
+		deltaTime = static_cast<float>(duration.count()) / 1000;
+		timer += deltaTime;
+		prevTime = end;
+	}
+
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+	std::chrono::time_point<std::chrono::high_resolution_clock> prevTime;
+public:
+	float timer = 0;
+	float deltaTime = 0;
 };
 
